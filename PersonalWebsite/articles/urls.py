32f5e2b2f2391dab15468da5f2f92e19781d9278
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-01-01 19:31:55 +0545

    Adds urls to add article and category

--- PersonalWebsite/articles/urls.py
+++ PersonalWebsite/articles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from articles.views import article_list, article_detail
+from articles.views import article_list, article_detail,\
+                        add_article, add_category
 
 urlpatterns = [
+    path('add_article/', add_article, name = 'AddArticle' ),
+    path('add_category/', add_category, name = 'AddCategory'),
     path('list/', article_list, name = 'ArticleList'),
     path('detail/<slug:slug>/', article_detail, name = 'ArticleDetail'),
 ]
