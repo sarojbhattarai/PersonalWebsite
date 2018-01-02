2cf077df8c3835143f998927ad7e4e3cc908e871
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-01-02 18:50:58 +0545

    Serve Staticfiles

--- PersonalWebsite/PersonalWebsite/settings/base.py
+++ PersonalWebsite/PersonalWebsite/settings/base.py
@@ -129,8 +129,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../static'),
+    os.path.join(BASE_DIR, "../static"),
+    '/var/www/static/',
 ]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
