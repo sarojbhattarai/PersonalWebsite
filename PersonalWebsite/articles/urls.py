6f7ce332652ab83c9432f85ceaad776fa7af34ba
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-01-02 18:14:04 +0545

    Adds urls for update category

--- PersonalWebsite/articles/urls.py
+++ PersonalWebsite/articles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from articles.views import article_list, article_detail,\
-                        add_article, add_category
+                        add_article, add_category, update_article
 
 urlpatterns = [
     path('add_article/', add_article, name = 'AddArticle' ),
+    path('update_article/<slug:slug>/', update_article, name='UpdateArticle' ),
     path('add_category/', add_category, name = 'AddCategory'),
     path('list/', article_list, name = 'ArticleList'),
     path('detail/<slug:slug>/', article_detail, name = 'ArticleDetail'),
