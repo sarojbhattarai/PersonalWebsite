5d67707a3d97de5a969072330be1406ccf45057d
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-01-06 21:14:45 +0545

    Adds django-crispy-forms

--- PersonalWebsite/contact_form/forms.py
+++ PersonalWebsite/contact_form/forms.py
@@ -1,9 +1,36 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from crispy_forms.layout import Field, Layout, Fieldset, ButtonHolder, Submit, HTML
+from crispy_forms.bootstrap import *
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
+    name = forms.CharField(
+        label = 'Your Name',
+        max_length = 50
+        )
+    email = forms.EmailField(
+        label = 'Your Favourite Email')
     subject = forms.CharField()
-    message = forms.CharField()
+    message = forms.CharField(
+        label = 'Your Message'
+        )
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Fieldset(
+                    "This is legend",
+                    'name',
+                    'email',
+                    'subject',
+                    'message'
+                ),
+            ButtonHolder(
+                Submit('submit', 'Submit', css_id = 'ContactFormSubmit'))
+
+            )
 
