b89b370fe3560267d7a6a3e1c4eb1972215702a6
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-01-06 20:54:11 +0545

    Adds views for contact form

--- PersonalWebsite/contact_form/views.py
+++ PersonalWebsite/contact_form/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from contact_form.forms import ContactForm
+from django.core.mail import EmailMessage
 # Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            sender_name = request.POST.get('name')
+            sender_email = request.POST.get('email')
+            subject = request.POST.get('subject')
+            message = request.POST.get('message')
+
+            email = EmailMessage(
+                    'Message From Contact Form',
+                    message, sender_email, ['shresthaNischalLal@gmail.com', 'aakrist666@gmail.com', 'hi@nischal.info.np'],
+                    headers  = {'Reply-To': sender_email}
+                )
+            email.send()
+
+            # if sender_name and sender_email and subject and message:
+            #     try:
+            #         send_mail(subject, message, sender_email, ['shresthaNischalLal@gmail.com'],
+            #         fail_silently=False,
+            #         )
+            #     except BadHeaderError:
+            #         return HttpResponse("Bad Header")
+            return render(request, 'contact_form/mail_received.html', {})
+    form = ContactForm()
+    return render(request, 'contact_form/contact_form.html', {'form':form})
+       
