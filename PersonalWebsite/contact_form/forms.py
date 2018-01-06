251ab336a2b5d5ea2066d2abc9431b04b46e486d
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-01-06 20:53:58 +0545

    Adds contact forms form

---
+++ PersonalWebsite/contact_form/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    subject = forms.CharField()
+    message = forms.CharField()
+
