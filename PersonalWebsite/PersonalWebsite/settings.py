e21f6d809d84440aef6ae9070149f9d69a593349
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-02-24 18:35:52 +0545

    Changes in manange.py

--- PersonalWebsite/PersonalWebsite/settings.py
+++ PersonalWebsite/PersonalWebsite/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'PersonalWebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "../static"),
+    os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
 
